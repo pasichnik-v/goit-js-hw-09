5562fb87c5b444cb7c320c6856dc819a5261efad
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-03-04 10:34:58 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,4 +5,42 @@
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
 // Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною(disabled).
 // 
-// 1. 
+// 1. Витягнути кнопки через Queryselector
+//  2.  зробити 2 слухача: 1 кнопка пуск, 2 кнопка стоп
+//  3. Callback Pusk = setInterval() вклад 
+// 4. Callback Stop = clearInterval()
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBg = document.querySelector('body');
+
+startBtn.addEventListener('click', changeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+function changeColor() {
+    console.log('зміна кольору розпочалася');
+}
+
+function stopChangeColor() {
+    console.log('Подія зупинилася');
+}
+
+
+
+
+
+
+
+// const bodyBg = document.body;
+
+// const changeColor = () => {
+//   valueColor.textContent = getRandomHexColor();
+//   bodyBg.style.backgroundColor = valueColor.textContent;
+// };
+// changeColorButton.addEventListener("click", changeColor);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
