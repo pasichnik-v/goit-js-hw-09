03499de66a5a144b57e765febb9abb2b0f7877bd
Pasichnyk Vasyl <pasichnik-v@ukr.net>
2023-03-04 13:17:03 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,43 +4,37 @@
 // кольору фону повинна зупинятися.
 // Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів. 
 // Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною(disabled).
-// 
-// 1. Витягнути кнопки через Queryselector
-//  2.  зробити 2 слухача: 1 кнопка пуск, 2 кнопка стоп
-//  3. Callback Pusk = setInterval() вклад 
-// 4. Callback Stop = clearInterval()
+
+// 1. Отриати доступ до кнопок
 
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
-const bodyBg = document.querySelector('body');
+// const bodyBg = document.querySelector('body');
+const bodyBg = document.body;
+let timer = null;
+stopBtn.disabled = true;
 
-startBtn.addEventListener('click', changeColor);
-stopBtn.addEventListener('click', stopChangeColor);
+//  2.  зробити 2 слухача: 1 кнопка пуск, 2 кнопка стоп
+startBtn.addEventListener('click', onStartChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
 
-function changeColor() {
-    console.log('зміна кольору розпочалася');
+//  3. Описуємо функцію запуску зміни кольорів
+function onStartChangeColor() {
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+    bodyBg.style.backgroundColor = getRandomHexColor();
+     timer = setInterval(() => {bodyBg.style.backgroundColor = getRandomHexColor();
+    }, 1000)
 }
-
-function stopChangeColor() {
-    console.log('Подія зупинилася');
+//  4. Описуємо функцію зупинки
+function onStopChangeColor() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+    clearInterval(timer);
 }
-
-
-
-
-
-
-
-// const bodyBg = document.body;
-
-// const changeColor = () => {
-//   valueColor.textContent = getRandomHexColor();
-//   bodyBg.style.backgroundColor = valueColor.textContent;
-// };
-// changeColorButton.addEventListener("click", changeColor);
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+//  5. Описуємо функцію рандомних кольорів
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
