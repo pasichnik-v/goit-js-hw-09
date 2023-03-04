46338e6b8069d3c238174c8989ce3fb752b7f2da
pasichnik-v <106420702+pasichnik-v@users.noreply.github.com>
2023-03-04 08:41:37 +0000

    Deploying to gh-pages from @ pasichnik-v/goit-js-hw-09@5562fb87c5b444cb7c320c6856dc819a5261efad 🚀

---
+++ 01-color-switcher.498c52cc.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){console.log("зміна кольору розпочалася")})),e.addEventListener("click",(function(){console.log("Подія зупинилася")}));
+//# sourceMappingURL=01-color-switcher.498c52cc.js.map
---
+++ 01-color-switcher.749b9251.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");t.addEventListener("click",(function(){console.log("зміна кольору розпочалася")})),e.addEventListener("click",(function(){console.log("Подія зупинилася")}))}();
+//# sourceMappingURL=01-color-switcher.749b9251.js.map
